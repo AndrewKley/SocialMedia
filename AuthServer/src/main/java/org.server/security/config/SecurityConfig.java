4fa6a4ec5b63f43ae7608a9443562869d72f90a7
ub <mudrovanddrey@gmail.com>
2023-07-13 20:00:48 +0300

    loads login page

--- AuthServer/src/main/java/org.server/security/config/SecurityConfig.java
+++ AuthServer/src/main/java/org.server/security/config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package org.server.security.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
